6bad7864010d02ddace67705093ff3d524d31838
Patrick Mwangi <frashid274@gmail.com>
2026-09-22 04:30:07 +0300

    Allow remote image hosts used by places and events.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "**.ticketm.net",
+      },
+      {
+        protocol: "https",
+        hostname: "media.ticketmaster.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.ticketmaster.eu",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
